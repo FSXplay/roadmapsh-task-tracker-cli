e32259ef14da28df847ceb00b171e49042402acc
FSXplay <skinoftheduck@gmail.com>
2026-02-04 23:19:02 +0800

    Implement TaskManager methods

--- src/model/TaskManager.java
+++ src/model/TaskManager.java
@@ -3,28 +3,48 @@ package model;
 import model.Task.Task;
 import model.Task.Status;
 
-public class TaskManager {
-    public void addTask(String taskName) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class TaskManager {
+    private static int idCount = 1;
+    private Map<Integer, Task> taskMap = new HashMap<>();
+
+    public void addTask(String description) {
+        taskMap.put(idCount, new Task(idCount, description));
+        idCount++;
     }
 
-    public void updateTask(int id, String newTaskName) {
-        
+    public void updateTask(int id, String newDescription) {
+        Task selectedTask = taskMap.get(id);
+        selectedTask.description = newDescription;   
     }
 
     public void deleteTask(int id) {
+        taskMap.remove(id);
     }
 
     public void markInProgress(int id) {
+        Task selectedTask = taskMap.get(id);
+        selectedTask.status = Status.IN_PROGRESS;
     }
 
     public void markDone(int id) {
+        Task selectedTask = taskMap.get(id);
+        selectedTask.status = Status.DONE;
     }
 
     public void listAll() {
+        for (Task task : taskMap.values()) {
+            System.out.println(task);
+        }
     }
 
     public void listByStatus(Status status) {
-        
+        for (Task task : taskMap.values()) {
+            if (task.status == status) {
+                System.out.println(task);
+            }
+        }
     }
 }

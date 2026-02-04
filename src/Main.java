925398590724807c24fa054429c373b6153a5564
FSXplay <skinoftheduck@gmail.com>
2026-02-04 18:20:00 +0800

    Separate a TaskManger class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import model.TaskManager;
+
 public class Main {
-    public static void main(String[] atgs) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            printUsage();
+            return;
+        }
+
+        TaskManager taskManager = new TaskManager();
+        String command = args[0];
+
+        switch (command) {
+            case "add":
+                taskManager.addTask(args[1]);
+                break;
+            case "update":
+
+                break;
+            case "delete":
+                break;
+            case "mark-in-progress":
+                break;
+            case "mark-done":
+                break;
+            case "list":
+                break;
+            case "list done":
+                break;
+            case "list todo":
+                break;
+            case "list in-progress":
+                break;
+
+            default:
+                System.out.println("Unknown command: " + command);
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("""
+                Usage:
+                  task-cli add "description"
+                  task-cli update <id> "new description"
+                  task-cli delete <id>
+                  task-cli mark-in-progress <id>
+                  task-cli mark-done <id>
+                  task-cli list
+                  task-cli list todo|done|in-progress
+                """);
     }
 }
---
+++ src/model/TaskManager.java
@@ -0,0 +1,30 @@
+package model;
+
+import model.Task.Task;
+import model.Task.Status;
+
+public class TaskManager {
+    public void addTask(String taskName) {
+
+    }
+
+    public void updateTask(int id, String newTaskName) {
+        
+    }
+
+    public void deleteTask(int id) {
+    }
+
+    public void markInProgress(int id) {
+    }
+
+    public void markDone(int id) {
+    }
+
+    public void listAll() {
+    }
+
+    public void listByStatus(Status status) {
+        
+    }
+}
